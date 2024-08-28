c7c93928aaba6b2eec91db28c9e34be56bbf0865
Praveen Gadwala <150447609+Praveengadwala06@users.noreply.github.com>
2024-08-28 11:16:21 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector(".nav"),
+ 
+navOpenBtn = document.querySelector(".navOpenBtn"),
+navCloseBtn = document.querySelector(".navCloseBtn");
+
+ 
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
